92683bbada5a0d3c7d536936f1e0d6264493915c
Karl F. A. Friebel <karl.friebel@friebelnet.de>
2026-05-22 18:39:43 +0200

    Added a work-around for DYLIB linking.
    When building with BUILD_SHARED_LIBS=OFF, MLIR will not set the RPATH
    of the installed Python CAPIs. However, when linking against the DSOs,
    it is necessary. This commit adds a work-around that allows for the
    installed DSOs to be found.

--- cmake/AddKTIRPython.cmake
+++ cmake/AddKTIRPython.cmake
@@ -16,3 +16,26 @@ if(NOT TARGET KTIRPythonSources.Dialects)
     ADD_TO_PARENT KTIRPythonSources
   )
 endif()
+
+# FIXME: This is a horrible work-around. Unfortunately, MLIR will not set the
+#        INSTALL_RPATH when BUILD_SHARED_LIBS is off, even though we need to
+#        link against the DSO. Since we're installing it into the lib/ dir, we
+#        fix it by doing it manually.
+function(ktir_python_fix_rpath target)
+  cmake_parse_arguments(ARG "" "RELATIVE_INSTALL_ROOT" "" ${ARGN})
+
+  if(LLVM_LINK_LLVM_DYLIB OR MLIR_LINK_MLIR_DYLIB)
+    if(APPLE)
+      set(_origin_prefix "@loader_path")
+    elseif(UNIX)
+      set(_origin_prefix "\$ORIGIN")
+    else()
+      return()
+    endif()
+
+    set_property(TARGET ${target} APPEND PROPERTY
+      INSTALL_RPATH 
+      "${_origin_prefix}/${ARG_RELATIVE_INSTALL_ROOT}/lib${LLVM_LIBDIR_SUFFIX}"
+    )
+  endif()
+endfunction()
